8843e3f9d1e7a45b712d290db8b8c2630640c081
Everett Vergara <evergara@shinra.com.ph>
2022-08-29 19:42:40 +0800

    simplified regular constructor for int/float

--- include/types/decimal.hpp
+++ include/types/decimal.hpp
@@ -55,38 +55,45 @@ namespace g80 {
         public:
 
             // Regular Constructor for integral types
-            template<typename I> requires std::is_integral<I>::value
-            decimal(const I i, const int8_t s = 0) : 
+            template<typename T> requires std::is_integral<T>::value || std::is_floating_point<T>::value
+            decimal(const T i, const int8_t s = 2) : 
                 scale_{s}, 
                 scale_mul_{get_scale_mul(scale_)}, 
-                data_{i * scale_mul_} {}
+                data_{static_cast<int64_t>(i * scale_mul_)} {}
 
-            // Regular Constructor for floating-points
-            template<typename F> requires std::is_floating_point<F>::value
-            decimal(const F f, const int8_t s = 6) : 
-                scale_{s}, 
-                scale_mul_{get_scale_mul(scale_)}, 
-                data_{static_cast<int64_t>(f * scale_mul_)} {}
+            // // Regular Constructor for floating-points
+            // template<typename F> requires std::is_floating_point<F>::value
+            // decimal(const F f, const int8_t s = 6) : 
+            //     scale_{s}, 
+            //     scale_mul_{get_scale_mul(scale_)}, 
+            //     data_{static_cast<int64_t>(f * scale_mul_)} {}
 
-            // Copy Constructor for decimal
-            template<typename D> requires std::is_same<D, decimal>::value
-            decimal(const D &d, const int8_t s) : 
-                scale_{s}, 
-                scale_mul_{get_scale_mul(s)}, 
-                data_{d.data_on_scale(s)} {
-            }
+            // // Copy Constructor for decimal
+            // template<typename D> requires std::is_same<D, decimal>::value
+            // decimal(const D &d, const int8_t s) : 
+            //     scale_{s}, 
+            //     scale_mul_{get_scale_mul(s)}, 
+            //     data_{d.data_on_scale(s)} {}
 
-            // Copy Constructor for decimal
-            template<typename D> requires std::is_same<D, decimal>::value
-            decimal(const D &d) : decimal{d, d.scale_} {
-            }
-
-
-//            decimal(const decimal &d, const int8_t scale) : data_{d.data_on_scale(scale)}, scale_{d.scale_}, scale_mul_{d.scale_mul_} {}
-            
-            // decimal(decimal &&r) : data_{r.data_}, scale_{r.scale_}, scale_mul_{r.scale_mul_} {}
+            // // Copy Constructor for decimal
+            // template<typename D> requires std::is_same<D, decimal>::value
+            // decimal(const D &d) : decimal{d, d.scale_} {
+            // }
 
             ~decimal() = default;
+
+            // // Assignment operator for integral and floating types
+            // template<typename T> requires std::is_integral<I>::value || std::is_floating_point<T>::value
+            // auto operator=(const T t) -> decimal & {
+            //     data_ = static_cast<int64_t>(t * scale_mul_);
+            // }
+
+            // // Assignment operator for decimal
+            // template<typename F> requires std::is_floating_point<I>::value
+            // auto operator=(const F f) -> decimal & {
+            //     data_ = i * scale_mul_;
+            // }
+
             
             // auto operator=(const decimal &r) -> decimal & {data_ = {r.data_}, scale_ = {r.scale_}, scale_mul_ = {r.scale_mul_}; return *this;}
             // auto operator=(decimal &&r) -> decimal & {data_ = {r.data_}, scale_ = {r.scale_}, scale_mul_ = {r.scale_mul_}; return *this;}
